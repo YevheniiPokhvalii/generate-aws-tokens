f828e5b5f400ad7f38fc17181b38ee3d4559ebc4
Yevhenii Pokhvalii <68227075+YevheniiPokhvalii@users.noreply.github.com>
2022-06-18 20:17:36 +0300

    Update aws_token.sh

--- aws_token.sh
+++ aws_token.sh
@@ -250,11 +250,11 @@ generate_aws_mfa()
 
    if [ -s "$aws_token_file" ]; then
       echo "$print_dashes"
-      aws_token_exp_utc="$(grep -o '\"Expiration\": "[^"]*' "$aws_token_file" | grep -o '[^"]*$')"
-      aws_token_exp_local="$(date -d "$aws_token_exp_utc" 2>/dev/null \
-      || date -jf "%Y-%m-%dT%X%z" "$(printf '%s' "$aws_token_exp_utc" | sed 's/\(.*\):/\1/')" +"%a %B %d %X %Z %Y" 2>/dev/null \
-      || printf '%s' "$aws_token_exp_utc")"
-      echo "The token expires on $aws_token_exp_local"
+      aws_token_exp_utc_time="$(grep -o '\"Expiration\": "[^"]*' "$aws_token_file" | grep -o '[^"]*$')"
+      aws_token_exp_local_time="$(date -d "$aws_token_exp_utc_time" 2>/dev/null \
+      || date -jf "%Y-%m-%dT%X%z" "$(printf '%s' "$aws_token_exp_utc_time" | sed 's/\(.*\):/\1/')" +"%a %B %d %X %Z %Y" 2>/dev/null \
+      || printf '%s' "$aws_token_exp_utc_time")"
+      echo "The token expires on $aws_token_exp_local_time"
    fi
 
    rm "$aws_token_file"
